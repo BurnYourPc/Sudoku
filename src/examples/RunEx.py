4e4d2b844366951ac48e6aca5e8a289d15b7f91e
TolisChal <tolis.chal@gmail.com>
2017-08-05 19:15:03 +0300

    Pdf, starting rating

--- src/examples/RunEx.py
+++ src/examples/RunEx.py
@@ -7,20 +7,39 @@ from src.solver import solver as SL
 from src.burnImage import burnSudo2Img as BS
 from src.generators import Generators as Gen
 from random import randint
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import os
 
 B1=np.array([[1,4,0,0,0,0,0,0,0],[0,0,5,4,7,1,0,0,0],[0,0,7,5,0,0,0,0,9],[0,6,1,7,0,5,3,8,0],[0,7,8,1,0,3,6,9,0],[0,5,9,6,0,8,7,2,0],[5,0,0,0,0,7,2,0,0],[0,0,0,3,8,4,1,0,0],[0,0,0,0,0,0,0,3,7]])
 B2=np.array([[0,0,0,0,9,0,7,0,8],[0,0,0,8,0,0,0,0,0],[5,1,0,6,0,7,0,0,0],[0,0,9,2,0,0,0,8,0],[6,0,0,0,0,0,0,0,4],[0,7,0,0,0,1,5,0,0],[0,0,0,5,0,9,0,6,1],[0,0,0,0,0,8,0,0,0],[2,0,3,0,7,0,0,0,0]])
 
 # Solve B1,B2 with the fastest solver implemented
-A1=SL.SudoSolveIt2(B1,[],1)
-A2=SL.SudoSolveIt2(B2,[],1)
+#A1=SL.SudoSolveIt2(B1,[],1)
+#A2=SL.SudoSolveIt2(B2,[],1)
 
 #Creating NonSymmetrical Problem with 23 known numbers
-matrix=Gen.RandGenerator3([],23,1,[])
-BS.CreateSudoImg(matrix,False)
+#matrix=Gen.RandGenerator3([],27,1,[])
+#BS.CreateSudoImg(matrix,False)
+
+#matrix=Gen.RandGenerator3([],28,1,[])
+#BS.CreateSudoImg(matrix,False)
+
+matrix=Gen.RandGenerator3([],17,1,[])
+BS.CreateSudoImg(matrix,False,"/home/tolis/Code-All/BurnYourPc/Sudoku/src/burnImage","/home/tolis/Code-All/BurnYourPc/Sudoku/SudoProblems/NonSymmetrical")
 
 #Creating Symmetrical Problems (symemetry is a naumber from 1 to 15, but 1-11 is efficient)
-matrix=Gen.GenSymmetrical(5)
-BS.CreateSudoImg(matrix,True)
-matrix=Gen.GenSymmetrical(2)
-BS.CreateSudoImg(matrix,True)
+#matrix=Gen.GenSymmetrical(5)
+#BS.CreateSudoImg(matrix,True)
+#matrix=Gen.GenSymmetrical(2)
+#BS.CreateSudoImg(matrix,True,"/home/tolis/Code-All/BurnYourPc/Sudoku/src/burnImage","/home/tolis/Code-All/BurnYourPc/Sudoku/SudoProblems/Symmetrical")
+#BS.CreateSudoPdf(32,'/home/tolis/Code-All/BurnYourPc/Sudoku/SudoProblems/NonSymmetrical',False,'/home/tolis/Code-All/BurnYourPc/Sudoku/src/examples','/home/tolis/Code-All/BurnYourPc/Sudoku/SudoProblems/SudoPdf')
+#os.remove(os.path.join('/home/tolis/Code-All/BurnYourPc/Sudoku', 'output1.pdf'))
+# Creating an object where pdf pages are appended to
+#output = PdfFileWriter()
+
+# Appending two pdf-pages from two different files
+#BS.append_pdf(PdfFileReader(open("output1.pdf","rb")),output)
+#BS.append_pdf(PdfFileReader(open("output2.pdf","rb")),output)
+
+# Writing all the collected pages to a file
+#output.write(open("CombinedPages.pdf","wb"))
